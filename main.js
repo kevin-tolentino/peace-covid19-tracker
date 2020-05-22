f31309e6f33c1cf4e683c5a2dc32adfc93827f5b
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-21 17:10:32 -0700

    got verse data to load using conditionals set on each getVerseSuccess method

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var h1 = document.querySelector("h1")
-var table = document.querySelector("table")
 var currentActive = document.getElementById("currentActive")
 var currentCritical = document.getElementById("currentCritical")
 var currentRecovered = document.getElementById("currentRecovered")
@@ -7,7 +6,7 @@ var currentDeaths = document.getElementById("currentDeaths")
 var verseText = document.getElementById("verseText")
 var verseRef = document.getElementById("verseRef")
 var verseArray = []
-var verseOfTheDay = null;
+// var verseOfTheDay = null;
 
 var currentDate = new Date();
 var currentYear = currentDate.getFullYear().toString();
@@ -72,6 +71,12 @@ function getVerseOneSearch() {
     url: "https://api.esv.org/v3/passage/search/?q=peace",
     headers: { "Authorization": "4bb2afad133ab4a9531a4be06fd06ae85703cf0f" },
     data: { "page-size": 100, "page": 1 },
+    beforeSend: function () {
+      if (today === 0){
+        verseText.textContent = 'loading'
+        verseRef.textContent = 'loading'
+      }
+    },
     success: handleGetVerseOneSuccess,
     error: handleGetVerseOneError
   })
@@ -83,6 +88,12 @@ function getVerseTwoSearch() {
     url: "https://api.esv.org/v3/passage/search/?q=peace",
     headers: { "Authorization": "4bb2afad133ab4a9531a4be06fd06ae85703cf0f" },
     data: { "page-size": 100, "page": 2 },
+    beforeSend: function () {
+      if (today === 1 || today === 2) {
+        verseText.textContent = 'loading'
+        verseRef.textContent = 'loading'
+      }
+    },
     success: handleGetVerseTwoSuccess,
     error: handleGetVerseTwoError
   })
@@ -94,6 +105,12 @@ function getVerseThreeSearch() {
     url: "https://api.esv.org/v3/passage/search/?q=peace",
     headers: { "Authorization": "4bb2afad133ab4a9531a4be06fd06ae85703cf0f" },
     data: { "page-size": 100, "page": 3 },
+    beforeSend: function () {
+      if (today === 3 || today === 4) {
+        verseText.textContent = 'loading'
+        verseRef.textContent = 'loading'
+      }
+    },
     success: handleGetVerseThreeSuccess,
     error: handleGetVerseThreeError
   })
@@ -105,6 +122,12 @@ function getVerseFourSearch() {
     url: "https://api.esv.org/v3/passage/search/?q=peace",
     headers: { "Authorization": "4bb2afad133ab4a9531a4be06fd06ae85703cf0f" },
     data: { "page-size": 100, "page": 4 },
+    beforeSend: function () {
+      if (today === 5 || today === 6) {
+        verseText.textContent = 'loading'
+        verseRef.textContent = 'loading'
+      }
+    },
     success: handleGetVerseFourSuccess,
     error: handleGetVerseFourError
   })
@@ -120,10 +143,9 @@ function getVerses(){
   getVerseFourSearch()
 }
 
-function verseOfTheDay() {
-  verseOfTheDay = verseArray[today]
-  verseText.textContent = verseOfTheDay.content
-  verseRef.textContent = verseOfTheDay.reference
+function verseOfTheDay(verseObject) {
+  verseText.textContent = verseObject.content
+  verseRef.textContent = verseObject.reference
 }
 
 function previousVerseOfTheDay(){
@@ -156,6 +178,7 @@ function handleGetVerseOneSuccess(data) {
   var reference = data.results[49].reference
   var numbers626 = {content, reference}
   verseArray[0] = numbers626
+  if (today === 0) {verseOfTheDay(numbers626)}
 }
 
 function handleGetVerseOneError(error) {
@@ -172,6 +195,9 @@ function handleGetVerseTwoSuccess(data) {
   reference = data.results[96].reference
   var isaiah263 = {content, reference }
   verseArray[2] = isaiah263
+  if (today === 1) { verseOfTheDay(psalm48) }
+  if (today === 2) { verseOfTheDay(isaiah263) }
+
 }
 
 function handleGetVerseTwoError(error) {
@@ -188,6 +214,8 @@ function handleGetVerseThreeSuccess(data) {
   reference = data.results[90].reference
   var romans51 = { content, reference }
   verseArray[4] = romans51
+  if (today === 3) { verseOfTheDay(john1427) }
+  if (today === 4) { verseOfTheDay(romans51) }
 }
 
 function handleGetVerseThreeError(error) {
@@ -204,6 +232,9 @@ function handleGetVerseFourSuccess(data) {
   reference = data.results[18].reference
   var colossians315 = { content, reference }
   verseArray[6] = colossians315
+  if (today === 5) { verseOfTheDay(philippians12) }
+  if (today === 6) { verseOfTheDay(colossians315) }
+
 }
 
 function handleGetVerseFourError(error) {
@@ -227,15 +258,8 @@ function updateCurrentCovidStats(data){
 
   covidHistory();
 
-  // getVerses();
+  getVerses();
 
-  // getVerseOneSearch()
-
-  // getVerseTwoSearch()
-
-  // getVerseThreeSearch()
-
-  // getVerseFourSearch()
 
 // }
 

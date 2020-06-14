90ae2b33cd89318f7fba1ab015c436a7f10b0c7d
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-13 21:39:08 -0700

    defined selector properties on the this object in constructor

--- components/covidTable.js
+++ components/covidTable.js
@@ -1,6 +1,14 @@
 class CovidTable {
-  constructor(verseText, verseRef) {
+  constructor(verseText, verseRef, currentActive, currentCritical, currentRecovered, currentDeaths) {
     this.verseText = verseText;
     this.verseRef = verseRef;
+    this.currentActive = currentActive
+    this.currentCritical = currentCritical
+    this.currentRecovered = currentRecovered
+    this.currentDeaths = currentDeaths
+  }
+
+  beforeSendCovidHistory(){
+
   }
 }

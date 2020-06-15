480c927acbfe4aa1483aed1cc36990ef013fa78b
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-15 16:43:12 -0700

    successfully got current verse to load from ajax call via oop

--- components/app.js
+++ components/app.js
@@ -132,23 +132,23 @@ class App{
 
 
 
-function handleGetCovidCurrentSuccess(data) {
-  updateCurrentCovidStats(data)
+ handleGetCovidCurrentSuccess(data) {
+  this.covidTable.updateCurrentCovidStats(data)
   this.leftButton.removeAttribute('disabled', '')
   this.rightButton.removeAttribute('disabled', '')
 
 }
 
-function handleGetCovidCurrentError(error) {
+ handleGetCovidCurrentError(error) {
   console.error(error)
 }
 
-function handleGetCovidHistorySuccess(data) {
-  PreviousDayCovidStats(data)
-  rightButton.removeAttribute('disabled', '')
+ handleGetCovidHistorySuccess(data) {
+  this.covidTable.previousDayCovidStats(data)
+  this.rightButton.removeAttribute('disabled', '')
 }
 
-function handleGetCovidHistoryError(error) {
+ handleGetCovidHistoryError(error) {
   console.error(error)
 }
 
@@ -157,24 +157,24 @@ handleGetVerseOneSuccess(data) {
   var reference = data.results[49].reference
   var numbers626 = { content, reference }
   this.verseArray[0] = numbers626
-  if (this.today === 0) { this.verseDisplay.verseOfTheDay(numbers626) }
+  if (this.today === 0) { this.verseDisplay.verseOfTheDay(numbers626, this.currentDate) }
 }
 
 handleGetVerseOneError(error) {
   console.error(error)
 }
 
-function handleGetVerseTwoSuccess(data) {
+ handleGetVerseTwoSuccess(data) {
   var content = data.results[65].content
   var reference = data.results[65].reference
   var psalm48 = { content, reference }
-  verseArray[1] = psalm48
+  this.verseArray[1] = psalm48
   content = data.results[96].content
   reference = data.results[96].reference
   var isaiah263 = { content, reference }
   this.verseArray[2] = isaiah263
-  if (this.today === 1) { this.verseDisplay.verseOfTheDay(psalm48) }
-  if (this.today === 2) { this.verseDisplay.verseOfTheDay(isaiah263) }
+  if (this.today === 1) { this.verseDisplay.verseOfTheDay(psalm48, this.currentDate) }
+  if (this.today === 2) { this.verseDisplay.verseOfTheDay(isaiah263, this.currentDate) }
 
 }
 
@@ -191,8 +191,8 @@ function handleGetVerseTwoSuccess(data) {
   reference = data.results[90].reference
   var romans51 = { content, reference }
   this.verseArray[4] = romans51
-  if (this.today === 3) { this.verseDisplay.verseOfTheDay(john1427) }
-  if (this.today === 4) { this.verseDisplay.verseOfTheDay(romans51) }
+  if (this.today === 3) { this.verseDisplay.verseOfTheDay(john1427, this.currentDate) }
+  if (this.today === 4) { this.verseDisplay.verseOfTheDay(romans51, this.currentDate) }
 }
 
  handleGetVerseThreeError(error) {
@@ -208,8 +208,8 @@ function handleGetVerseTwoSuccess(data) {
   reference = data.results[18].reference
   var colossians315 = { content, reference }
   this.verseArray[6] = colossians315
-  if (this.today === 5) { this.verseDisplay.verseOfTheDay(philippians12) }
-  if (this.today === 6) { this.verseDisplay.verseOfTheDay(colossians315) }
+  if (this.today === 5) { this.verseDisplay.verseOfTheDay(philippians12, this.currentDate) }
+  if (this.today === 6) { this.verseDisplay.verseOfTheDay(colossians315, this.currentDate) }
 
 }
 
@@ -218,82 +218,82 @@ function handleGetVerseTwoSuccess(data) {
 }
 
 
-function getPreviousDay() {
-  this.leftButton.classList.add('invisible')
-  this.middleButton.classList.add('invisible')
-  this.covidHistory();
-  previousVerseOfTheDay(this.verseArray, this.previousDayDate, this.yesterday)
-  this.rightButton.textContent = 'View Current Day'
-  this.rightButton.removeEventListener('click', getPreviewDay)
-  this.rightButton.addEventListener('click', covidCurrent)
-  this.rightButton.addEventListener('click', viewCurrentDayRightButton)
-}
+// function getPreviousDay() {
+//   this.leftButton.classList.add('invisible')
+//   this.middleButton.classList.add('invisible')
+//   this.covidHistory();
+//   previousVerseOfTheDay(this.verseArray, this.previousDayDate, this.yesterday)
+//   this.rightButton.textContent = 'View Current Day'
+//   this.rightButton.removeEventListener('click', getPreviewDay)
+//   this.rightButton.addEventListener('click', covidCurrent)
+//   this.rightButton.addEventListener('click', viewCurrentDayRightButton)
+// }
 
-function getPreviewDay() {
-  middleButton.classList.add('invisible')
-  rightButton.classList.add('invisible')
-  previewVerseOfTheDay(this.verseArray, this.previewDayDate, this.tomorrow)
-  leftButton.textContent = 'View Current Day'
-  leftButton.removeEventListener('click', getPreviousDay)
-  leftButton.addEventListener('click', viewCurrentDayLeftButton)
-  currentActive.textContent = 'TBD'
-  currentCritical.textContent = 'TBD'
-  currentRecovered.textContent = 'TBD'
-  currentDeaths.textContent = 'TBD'
+// function getPreviewDay() {
+//   middleButton.classList.add('invisible')
+//   rightButton.classList.add('invisible')
+//   previewVerseOfTheDay(this.verseArray, this.previewDayDate, this.tomorrow)
+//   leftButton.textContent = 'View Current Day'
+//   leftButton.removeEventListener('click', getPreviousDay)
+//   leftButton.addEventListener('click', viewCurrentDayLeftButton)
+//   currentActive.textContent = 'TBD'
+//   currentCritical.textContent = 'TBD'
+//   currentRecovered.textContent = 'TBD'
+//   currentDeaths.textContent = 'TBD'
 
-}
+// }
 
-function viewCurrentDayRightButton() {
-  leftButton.classList.remove('invisible')
-  middleButton.classList.remove('invisible')
-  rightButton.removeEventListener('click', covidCurrent)
-  rightButton.removeEventListener('click', viewCurrentDayRightButton)
-  verseOfTheDay(verseArray[today])
-  rightButton.textContent = "Preview Tomorrow's Verse"
-  rightButton.addEventListener('click', getPreviewDay)
+// function viewCurrentDayRightButton() {
+//   leftButton.classList.remove('invisible')
+//   middleButton.classList.remove('invisible')
+//   rightButton.removeEventListener('click', covidCurrent)
+//   rightButton.removeEventListener('click', viewCurrentDayRightButton)
+//   verseOfTheDay(verseArray[today])
+//   rightButton.textContent = "Preview Tomorrow's Verse"
+//   rightButton.addEventListener('click', getPreviewDay)
 
-}
+// }
 
-function viewCurrentDayLeftButton() {
-  rightButton.classList.remove('invisible')
-  middleButton.classList.remove('invisible')
-  leftButton.removeEventListener('click', covidCurrent)
-  leftButton.removeEventListener('click', viewCurrentDayLeftButton)
-  verseOfTheDay(verseArray[today])
-  covidCurrent()
-  leftButton.textContent = 'Previous Day'
-  leftButton.addEventListener("click", getPreviousDay)
-}
+// function viewCurrentDayLeftButton() {
+//   rightButton.classList.remove('invisible')
+//   middleButton.classList.remove('invisible')
+//   leftButton.removeEventListener('click', covidCurrent)
+//   leftButton.removeEventListener('click', viewCurrentDayLeftButton)
+//   verseOfTheDay(verseArray[today])
+//   covidCurrent()
+//   leftButton.textContent = 'Previous Day'
+//   leftButton.addEventListener("click", getPreviousDay)
+// }
 
- startTimer(duration, display) {
-  middleButton.setAttribute('disabled', '')
-  var timer = duration, minutes, seconds;
-  var intervalId = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
+//  startTimer(duration, display) {
+//   middleButton.setAttribute('disabled', '')
+//   var timer = duration, minutes, seconds;
+//   var intervalId = setInterval(function () {
+//     minutes = parseInt(timer / 60, 10);
+//     seconds = parseInt(timer % 60, 10);
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+//     minutes = minutes < 10 ? "0" + minutes : minutes;
+//     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    display.textContent = minutes + ":" + seconds;
-    if (minutes === "00" && seconds === "00") {
-      middleButton.removeAttribute('disabled', '')
-      clearInterval(intervalId)
-      display.textContent = null
-    }
+//     display.textContent = minutes + ":" + seconds;
+//     if (minutes === "00" && seconds === "00") {
+//       middleButton.removeAttribute('disabled', '')
+//       clearInterval(intervalId)
+//       display.textContent = null
+//     }
 
-    if (--timer < 0) {
-      timer = duration;
-    }
-    if (timer === 0) {
-      display.textContent = minutes + ":" + seconds
-    }
-  }, 1000);
-}
+//     if (--timer < 0) {
+//       timer = duration;
+//     }
+//     if (timer === 0) {
+//       display.textContent = minutes + ":" + seconds
+//     }
+//   }, 1000);
+// }
 
   start(){
   this.getVerses()
-  this.startTimer(900, this.timer)
+  // this.startTimer(900, this.timer)
 
 }
 

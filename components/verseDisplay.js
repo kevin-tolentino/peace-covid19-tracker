daaa55ce1944f4060f71d1a48565d58442ddd35a
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-15 12:46:05 -0700

    moved displayVerse methods to verseDisplay class

--- components/verseDisplay.js
+++ components/verseDisplay.js
@@ -1,11 +1,38 @@
 class VerseDisplay{
-  constructor(verseText, verseRef){
+  constructor(verseText, verseRef, dateHeader){
     this.verseText = verseText
     this.verseRef = verseRef
+    this.dateHeader = dateHeader
   }
 
   beforeSendVerse(){
       this.verseText.textContent = 'loading'
       this.verseRef.textContent = 'loading'
   }
+
+  previousVerseOfTheDay() {
+  dateHeader.textContent = previousDayDate.toDateString()
+  var previousVerseInfo = verseArray[yesterday]
+  var formattedText = previousVerseInfo.content
+  formattedText = formattedText[0].toUpperCase() + formattedText.slice(1)
+  verseText.textContent = formattedText
+  verseRef.textContent = previousVerseInfo.reference
+}
+
+verseOfTheDay(verseObject) {
+  dateHeader.textContent = currentDate.toDateString()
+  var formattedText = verseObject.content
+  formattedText = formattedText[0].toUpperCase() + formattedText.slice(1)
+  verseText.textContent = formattedText
+  verseRef.textContent = verseObject.reference
+}
+
+previewVerseOfTheDay() {
+  dateHeader.textContent = previewDayDate.toDateString()
+  var previewVerseInfo = verseArray[tomorrow]
+  var formattedText = previewVerseInfo.content
+  formattedText = formattedText[0].toUpperCase() + formattedText.slice(1)
+  verseText.textContent = formattedText
+  verseRef.textContent = previewVerseInfo.reference
+}
 }

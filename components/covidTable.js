e6e40eab1d6f4fde8e2d5dc982d1e9cec3b1b0c2
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-27 13:56:28 -0700

    added new variables to constructor of table

--- components/covidTable.js
+++ components/covidTable.js
@@ -1,11 +1,26 @@
 class CovidTable {
-  constructor(table, errorMessage, currentActive, currentCritical, currentRecovered, currentDeaths, tested, confirmedCases, newCases, newDeaths, totalPopulation) {
+  constructor(table,
+    errorMessage,
+    currentActive,
+    currentCritical,
+    currentRecovered,
+    currentDeaths,
+    tested,
+    confirmedCases,
+    newCases,
+    newDeaths,
+    totalPopulation) {
     this.table = table
     this.errorMessage = errorMessage
     this.currentActive = currentActive
     this.currentCritical = currentCritical
     this.currentRecovered = currentRecovered
     this.currentDeaths = currentDeaths
+    this.tested = tested
+    this.confirmedCases = confirmedCases
+    this.newCases = newCases
+    this.newDeaths = newDeaths
+    this.totalPopulation = totalPopulation
   }
 
 
@@ -20,14 +35,17 @@ class CovidTable {
 
   beforeSendCovid() {
     this.currentActive.textContent = 'loading'
-    this.currentCritical.textContent = 'loading'
     this.currentRecovered.textContent = 'loading'
     this.currentDeaths.textContent = 'loading'
+    this.tested.textContent = 'loading'
+    this.confirmedCases.textContent = 'loading'
+    this.newCases.textContent = 'loading'
+    this.newDeaths.textContent = 'loading'
+    this.totalPopulation.textContent = 'loading'
   }
 
   previousDayCovidStats(data) {
     this.currentActive.textContent = this.formatNumber(data.response[0].cases.active)
-    this.currentCritical.textContent = this.formatNumber(data.response[0].cases.critical)
     this.currentRecovered.textContent = this.formatNumber(data.response[0].cases.recovered)
     this.currentDeaths.textContent = this.formatNumber(data.response[0].deaths.total)
   }

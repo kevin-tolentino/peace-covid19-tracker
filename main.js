078e3a84feacd01df9b85720007015901eb43792
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-27 20:18:20 -0700

    changed variables to es6

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const retryCurrentGet = document.getElementById('retryCurrentGet')
 const retryHistoryGet = document.getElementById('retryHistoryGet')
 const footer = document.getElementById('footer')
-var currentActive = document.getElementById("currentActive")
-var currentRecovered = document.getElementById("currentRecovered")
-var currentDeaths = document.getElementById("currentDeaths")
+const currentActive = document.getElementById("currentActive")
+const currentRecovered = document.getElementById("currentRecovered")
+const currentDeaths = document.getElementById("currentDeaths")
 const verseDisplayDiv = document.getElementById("verseDisplay")
 const verseErrorMessage = document.getElementById('verseErrorMessage')
 const retryVerseGet = document.getElementById('retryVerseGet')
-var verseText = document.getElementById("verseText")
-var verseRef = document.getElementById("verseRef")
-var dateHeader = document.getElementById('date')
-var timer = document.getElementById('timer')
-var leftButton = document.getElementById('leftButton')
-var currentRightDay = document.getElementById('currentRightDay')
-var middleButton = document.getElementById('middleButton')
-var rightButton = document.getElementById('rightButton')
-var currentLeftDay = document.getElementById('currentLeftDay')
+const verseText = document.getElementById("verseText")
+const verseRef = document.getElementById("verseRef")
+const dateHeader = document.getElementById('date')
+const timer = document.getElementById('timer')
+const leftButton = document.getElementById('leftButton')
+const currentRightDay = document.getElementById('currentRightDay')
+const middleButton = document.getElementById('middleButton')
+const rightButton = document.getElementById('rightButton')
+const currentLeftDay = document.getElementById('currentLeftDay')
 const currentErrorMessage = document.getElementById('currentErrorMessage')
 const historyErrorMessage = document.getElementById('historyErrorMessage')
 const table = document.getElementById('table')
@@ -24,28 +24,28 @@ const confirmedCases = document.getElementById('confirmedCases')
 const newCases = document.getElementById('newCases')
 const newDeaths = document.getElementById('newDeaths')
 const totalPopulation = document.getElementById('totalPopulation')
-var currentDate = new Date();
-var previousDayDate = new Date();
+const currentDate = new Date();
+let previousDayDate = new Date();
 previousDayDate.setDate(previousDayDate.getDate() - 1);
-var previewDayDate = new Date()
+let previewDayDate = new Date()
 previewDayDate.setDate(previewDayDate.getDate() + 1);
-var yesterdayYear = previousDayDate.getFullYear().toString();
-var yesterdayMonth = (previousDayDate.getMonth() + 1).toString();
+let yesterdayYear = previousDayDate.getFullYear().toString();
+let yesterdayMonth = (previousDayDate.getMonth() + 1).toString();
 
 if (yesterdayMonth.length === 1) {
   yesterdayMonth = '0' + yesterdayMonth;
 }
-var yesterdayDay = (previousDayDate.getDate().toString());
+let yesterdayDay = (previousDayDate.getDate().toString());
 if (yesterdayDay.length === 1) {
   yesterdayDay = '0' + yesterdayDay;
 }
-var formattedPreviousDate = `${yesterdayYear}-${yesterdayMonth}-${yesterdayDay}`;
+const formattedPreviousDate = `${yesterdayYear}-${yesterdayMonth}-${yesterdayDay}`;
 
-var today = currentDate.getDay()
-var yesterday = today - 1
-var tomorrow = (today === 6) ? 0 : (today + 1)
+const today = currentDate.getDay()
+const yesterday = today - 1
+const tomorrow = (today === 6) ? 0 : (today + 1)
 
 const verseDisplay = new VerseDisplay(verseDisplayDiv, verseErrorMessage, verseText, verseRef, dateHeader, currentDate)
 const covidTable = new CovidTable(table, currentErrorMessage, historyErrorMessage, currentActive, currentRecovered, currentDeaths, tested, confirmedCases, newCases, newDeaths, totalPopulation)
-var covidTracker = new App(retryCurrentGet, retryHistoryGet, retryVerseGet, footer, verseDisplay, covidTable, formattedPreviousDate, timer, previousDayDate, currentDate, previewDayDate, yesterday, today, tomorrow, currentRightDay, leftButton, middleButton, rightButton, currentLeftDay)
+const covidTracker = new App(retryCurrentGet, retryHistoryGet, retryVerseGet, footer, verseDisplay, covidTable, formattedPreviousDate, timer, previousDayDate, currentDate, previewDayDate, yesterday, today, tomorrow, currentRightDay, leftButton, middleButton, rightButton, currentLeftDay)
 covidTracker.start()
